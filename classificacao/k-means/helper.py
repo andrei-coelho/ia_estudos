98622f2ee87cc9d77ce64d65260c458462fcecce
Andrei Coelho <andreifcoelho@gmail.com>
2026-02-04 20:31:35 -0300

    inicio do kmeans

---
+++ classificacao/k-means/helper.py
@@ -0,0 +1,81 @@
+import hashlib
+import math
+import os
+import json
+import re
+
+import unicodedata
+
+def remover_acentos(texto):
+    processado = unicodedata.normalize('NFD', texto)
+    return "".join(c for c in processado if unicodedata.category(c) != 'Mn')
+
+
+def get_words_in_text(text):
+    
+    text = remover_acentos(text)
+    words = text.strip().lower().split()
+    padrao = r"[^\d\s:\-0-9\+]+"
+    palavras_validas = []
+
+    for word in words:
+        if(len(word) < 4):
+            continue 
+        if re.fullmatch(padrao, word):
+            palavras_validas.append(word)
+
+    return palavras_validas
+
+
+def get_json_file(caminho):
+    try:
+        with open(caminho, 'r') as file:
+            data = file.readlines()
+            jdata = [json.loads(item) for item in data]
+            return jdata
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
+def aumentar_densidade_feature_e_transmormar_em_vetor(val, dim=200):
+   
+    val_encode = val.strip().lower().encode("utf-8")
+    seed = int(hashlib.sha256(val_encode).hexdigest(), 16)
+
+    vetor = [0.0] * dim
+
+    a = 1664525
+    c = 1013904223
+    m = 2**32
+
+    atual = seed % m
+    for i in range(dim):
+        atual = (a * atual + c) % m
+        vetor[i] = (atual / m) * 2.0 - 1.0
+
+    return vetor
+
+
+def calcular_modulo_vetor(vetor):
+    return math.sqrt(sum(v * v for v in vetor))
+
+
+def somar_vetor(a, b):
+    return [x + y for x, y in zip(a, b)]
+
+
+def subtracao_vetor(a, b):
+    return [x - y for x, y in zip(a, b)]
+
+
+def multiply_vetor(a, b):
+    if len(a) != len(b):
+        print("Vetor A: {}".format(a))
+        print("Vetor B: {}".format(b))
+        raise ValueError("Vetores devem ter o mesmo tamanho. Tamnho a: {}, Tamanho b: {}".format(len(a), len(b)))
+    return sum(x * y for x, y in zip(a, b))
+
+
+def multiplicar_por_escalar(escalar, vetor):
+    return [escalar * x for x in vetor]
---
+++ classificacao/k-means/preprocess.py
@@ -0,0 +1,93 @@
+import json
+import helper
+import math
+import random
+
+# APENAS ORGANIZANDO O DATASET INICIAL 
+
+def gerar_dataset_formatado():
+    with open('../../dummy_datas/produtos_compilados.json', 'r') as file:
+        
+        lista = json.loads(file.read())
+        
+        palavras = []
+        data = []
+        
+        for item in lista:
+            data.append(helper.remover_acentos(item['descricao']))
+            palavras += helper.get_words_in_text(item['descricao'])
+
+    palavras_unicas = list(set(palavras))
+
+    with open('palavras_unicas.json', 'w') as f1:
+        json.dump(palavras_unicas, f1, ensure_ascii=False)
+        f1.close()
+
+    with open('data_set.json', 'w') as f2:
+        json.dump(data, f2, ensure_ascii=False)
+        f2.close()
+
+
+def gerar_idf(dataset, dicionario):
+    
+    df = {}
+    totaldocs = len(dataset)
+    
+    for data in dataset:
+        termos = helper.get_words_in_text(data)
+        for termo in set(termos):
+            if termo not in df:
+                df[termo] = 0
+            df[termo] += 1
+
+    idf = {}
+    for termo in dicionario:
+        idf[termo] = math.log( (totaldocs + 1) / (df.get(termo, 0) + 1) )
+
+    return idf
+
+
+def vetorizar_modelo():
+
+    dataset    = []
+    dicionario = []
+
+    with open('data_set.json', 'r') as file:
+        dataset = json.loads(file.read())
+
+    with open('palavras_unicas.json', 'r') as file:
+        dicionario = json.loads(file.read())
+
+    idf = gerar_idf(dataset, dicionario)
+
+    vetores = []
+    ctermos = {}
+
+    for item in dicionario:
+        ctermos[item] = 0
+
+    tovector = []
+
+    for data in dataset:
+        tcterm = ctermos.copy() 
+        termos = helper.get_words_in_text(data)
+        for term in termos:
+            if term in dicionario:
+                tcterm[term] += 1
+        tovector.append(tcterm)
+
+    for vect in tovector:
+        vector = []
+        for termo in dicionario:
+            v = 0
+            if vect[termo] > 0: 
+                v = vect[termo] * idf[termo] 
+            vector.append(v)
+        vetores.append(vector)
+
+    random.shuffle(vetores)
+
+    with open('modelo.json', 'w') as file:
+        json.dump(vetores, file, ensure_ascii=False)
+
+vetorizar_modelo()
